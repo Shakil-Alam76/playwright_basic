6088c488e33c00b5f880b6f47593c3a2ed16dc06
Shanto <shakil.shanto76@gmail.com>
2024-06-26 23:54:35 +0600

    nothing

--- tests/homepage_test.spec.js
+++ tests/homepage_test.spec.js
@@ -8,14 +8,18 @@ test ('Home Page', async({page})=>{
 
     await expect(page).toHaveTitle("STORE");
 
-    //another line for git practice in main branch 
+    //another line for git practice in staging for conflicting
 
     const pageUrl=await page.url();
     console.log("Page url: ", pageUrl);
 
     await expect(page).toHaveURL("https://www.demoblaze.com/");
 
+<<<<<<< HEAD
     //added this line for git conflict
+=======
+    //added this line for conflicting practice
+>>>>>>> staging
 
     await page.close();
 
